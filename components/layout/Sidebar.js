eea65e68297468400562d77c14481ca69dae30bf
S7eezy <cvrc.valentin@gmail.com>
2023-02-08 12:15:11 +0100

    Side bar indexes

--- components/layout/Sidebar.js
+++ components/layout/Sidebar.js
@@ -32,53 +32,35 @@ const Sidebar = ({ openClass }) => {
                                         <li className={isActive.key == 1 ? "has-children active" : "has-children"} onClick={() => handleToggle(1)}>
                                             <span className="menu-expand"><i className="fi-rr-caret-down"></i></span>
                                             <Link href="/">Home</Link>
-                                            <ul className={isActive.key == 1 ? "sub-menu d-block" : "sub-menu d-none"}>
-                                                <li><Link href="/">Homepage - 1</Link></li>
-                                                <li><Link href="/index-2">Homepage - 2</Link></li>
-                                                <li><Link href="/index-3">Homepage - 3</Link></li>
-                                                <li><Link href="/index-4">Homepage - 4</Link></li>
-                                            </ul>
                                         </li>
                                         <li className={isActive.key == 2 ? "has-children active" : "has-children"} onClick={() => handleToggle(2)}>
                                             <span className="menu-expand"><i className="fi-rr-caret-down"></i></span>
-                                            <Link href="/#">Category</Link>
-                                            <ul className={isActive.key == 2 ? "sub-menu d-block" : "sub-menu d-none"}>
-                                                <li><Link href="/blog-archive">Blog Category 1</Link></li>
-                                                <li><Link href="/blog-archive-2">Blog Category 2</Link></li>
-                                                <li><Link href="/blog-archive-3">Blog Category 3</Link></li>
-                                                <li><Link href="/blog-archive-4">Blog Category 4</Link></li>
-                                                <li><Link href="/blog-archive-5">Blog Category 5</Link></li>
-                                            </ul>
+                                            <Link href="/about">About me</Link>
                                         </li>
                                         <li className={isActive.key == 3 ? "has-children active" : "has-children"} onClick={() => handleToggle(3)}>
                                             <span className="menu-expand"><i className="fi-rr-caret-down"></i></span>
-                                            <Link href="/#">Single Post</Link>
+                                            <Link href="/#">Code</Link>
                                             <ul className={isActive.key == 3 ? "sub-menu d-block" : "sub-menu d-none"}>
-                                                <li><Link href="/single-sidebar">Blog Single 1</Link></li>
-                                                <li><Link href="/single-no-sidebar">Blog Single 2</Link></li>
-                                                <li><Link href="/single-center">Blog Single 3</Link></li>
+                                                <li><Link href="/python">Python</Link></li>
+                                                <li><Link href="/js">JS</Link></li>
                                             </ul>
                                         </li>
-                                        <li className={isActive.key == 5 ? "has-children active" : "has-children"} onClick={() => handleToggle(5)}>
+                                        <li className={isActive.key == 4 ? "has-children active" : "has-children"} onClick={() => handleToggle(5)}>
                                         <span className="menu-expand"><i className="fi-rr-caret-down"></i></span>
-                                            <Link class="color-gray-500" href="/page-portfolio">Portfolio</Link>
-                                            <ul className={isActive.key == 5 ? "sub-menu d-block" : "sub-menu d-none"}>
-                                                <li><Link class="color-gray-500" href="/page-portfolio">My Portfolio</Link></li>
-                                                <li><Link class="color-gray-500" href="/page-portfolio-2">My Portfolio 2</Link></li>
-                                                <li><Link class="color-gray-500" href="/portfolio-details">Portfolio Details</Link></li>
+                                            <Link class="color-gray-500" href="/page-portfolio">System</Link>
+                                            <ul className={isActive.key == 4 ? "sub-menu d-block" : "sub-menu d-none"}>
+                                                <li><Link class="color-gray-500" href="/page-portfolio">Linux</Link></li>
+                                                <li><Link class="color-gray-500" href="/page-portfolio-2">NVIDIA Jetson</Link></li>
+                                                <li><Link class="color-gray-500" href="/portfolio-details">Windows</Link></li>
                                             </ul>
                                         </li>
-                                        <li className={isActive.key == 4 ? "has-children active" : "has-children"} onClick={() => handleToggle(4)}>
+                                        <li className={isActive.key == 5 ? "has-children active" : "has-children"} onClick={() => handleToggle(4)}>
                                             <span className="menu-expand"><i className="fi-rr-caret-down"></i></span>
-                                            <Link href="/#">Pages</Link>
-                                            <ul className={isActive.key == 4 ? "sub-menu d-block" : "sub-menu d-none"}>
-                                                <li><Link href="/page-about">About</Link></li>
-                                                <li><Link href="/page-author">Author posts</Link></li>
-                                                <li><Link href="/page-contact">Contact</Link></li>
-                                                <li><Link href="/page-search">Search results</Link></li>
-                                                <li><Link href="/page-login">Login</Link></li>
-                                                <li><Link href="/page-signup">Signup</Link></li>
-                                                <li><Link href="/page-404">Page 404</Link></li>
+                                            <Link href="/#">Articles</Link>
+                                            <ul className={isActive.key == 5 ? "sub-menu d-block" : "sub-menu d-none"}>
+                                                <li><Link href="/page-about">AI</Link></li>
+                                                <li><Link href="/page-author">Software</Link></li>
+                                                <li><Link href="/page-contact">Hardware</Link></li>
                                             </ul>
                                         </li>
                                         <li><Link href="/page-contact">Contact</Link></li>
